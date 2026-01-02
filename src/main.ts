3d04c9b4e75cc79cfd34ac7deb8dc769ba34048b
humza-uddin <thereal.humza@gmail.com>
2026-01-02 22:56:25 +0500

    undo Lane's giant mistake

--- src/main.ts
+++ src/main.ts
@@ -44,6 +44,12 @@ v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
 
-app.listen(config.api.port, () => {
+ app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
+ 
+/* const port = process.env.PORT || 8080;
+
+app.listen(Number(port), () => {
+  console.log(`Server running on port ${port}`);
+}); */
